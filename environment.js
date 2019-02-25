252b6ad5e78d9c65249477684045c507f52b6972
Phentom <phentom.net@gmail.com>
2019-02-25 22:20:24 +0000

    removed parseInt from env

--- environment.js
+++ environment.js
@@ -1,5 +1,5 @@
 module.exports = {
-  PORT: parseInt(process.env.PORT, 4) || 3000,
+  PORT: process.env.PORT || 3000,
   LOG_LEVEL: process.env.LOG_LEVEL || 'debug',
   DB_CONNECTION: process.env.DB_CONNECTION || 'mongodb://localhost/atrasos-api',
   ACCESS_KEY: process.env.ACCESS_KEY || 'dummy_key',
